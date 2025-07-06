a31764dd70ee97169d9cfa8878d24297ebaf9ddf
Miracle Friday <miraclef60@gmail.com>
2025-07-06 05:35:08 +0100

    refactor: Update colors

--- apps/mobile/src/theme/colors.ts
+++ apps/mobile/src/theme/colors.ts
@@ -12,15 +12,15 @@ const palette = {
   primary100: "#F4E0D9",
   primary200: "#E8C1B4",
   primary300: "#DDA28E",
-  primary400: "#D28468",
-  primary500: "#C76542",
-  primary600: "#A54F31",
+  primary400: "#02EBF5",
+  primary500: "#02EBF0",
+  primary600: "#00BCD4",
 
   secondary100: "#DCDDE9",
   secondary200: "#BCC0D6",
   secondary300: "#9196B9",
   secondary400: "#626894",
-  secondary500: "#41476E",
+  secondary500: "#080034",
 
   accent100: "#FFEED4",
   accent200: "#FFE1B2",
@@ -57,7 +57,7 @@ export const colors = {
   /**
    * The default color of the screen background.
    */
-  background: palette.neutral200,
+  background: palette.neutral100,
   /**
    * The default border color.
    */
