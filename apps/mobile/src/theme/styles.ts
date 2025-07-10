422c87c07259777a1e6428013efa3ab94183b1c0
Miracle Friday <miraclef60@gmail.com>
2025-07-10 10:09:01 +0100

    refactor: Update `styles.ts`

--- apps/mobile/src/theme/styles.ts
+++ apps/mobile/src/theme/styles.ts
@@ -3,6 +3,7 @@ import { ImageStyle, TextStyle, ViewStyle } from "react-native"
 /* Use this file to define styles that are used in multiple places in your app. */
 export const $styles = {
   row: { flexDirection: "row" } as ViewStyle,
+  alignCenter: { alignItems: "center" } as ViewStyle,
   flex1: { flex: 1 } as ViewStyle,
   flexWrap: { flexWrap: "wrap" } as ViewStyle,
 
