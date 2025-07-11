c01baa30ab1e087eed9531ffdbf3484c629f5fda
Miracle Friday <miraclef60@gmail.com>
2025-07-11 12:47:42 +0100

    feat: Explore screen sticky sliver

--- apps/mobile/src/components/StickyHeader/Explore.tsx
+++ apps/mobile/src/components/StickyHeader/Explore.tsx
@@ -1,12 +1,26 @@
-import { View } from "react-native"
+import { ImageStyle, View, ViewStyle } from "react-native"
 import { AutoImage } from "../AutoImage"
+import { ThemedStyle } from "@/theme"
+import { useAppTheme } from "@/utils/useAppTheme"
 
 const passryLogoText = require("../../../assets/images/passry-logo-text.png")
 
 export default function ExploreStickyHeader() {
+  const { themed } = useAppTheme()
+
   return (
-    <View>
-      <AutoImage src={passryLogoText} />
+    <View style={themed($stickyContainerStyle)}>
+      <AutoImage src={passryLogoText} style={$passryLogoTextStyle} />
     </View>
   )
 }
+
+const $passryLogoTextStyle: ImageStyle = {
+  position: "absolute",
+  top: 2,
+  left: 10,
+}
+
+const $stickyContainerStyle: ThemedStyle<ViewStyle> = ({ colors }) => ({
+  backgroundColor: colors.background,
+})
