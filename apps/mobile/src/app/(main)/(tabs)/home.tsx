9202c3363f51213a656833c10bd809cdef8204bb
Miracle Friday <miraclef60@gmail.com>
2025-07-26 14:13:50 +0100

    refactor: Add sticky header

--- apps/mobile/src/app/(main)/(tabs)/home.tsx
+++ apps/mobile/src/app/(main)/(tabs)/home.tsx
@@ -1,8 +1,10 @@
-import { Button, Screen, Text, PressableIcon } from "@/components"
+import { Screen, Text } from "@/components"
 import ExploreStickyHeader from "@/components/StickyHeader/Explore"
 import { ThemedStyle } from "@/theme"
 import { useAppTheme } from "@/utils/useAppTheme"
-import { ViewStyle } from "react-native"
+import { View, ViewStyle } from "react-native"
+
+const passryLogoText = require("../../../../assets/images/passry-logo-text.png")
 
 export default function HomeScreen() {
   const { themed } = useAppTheme()
@@ -11,15 +13,23 @@ export default function HomeScreen() {
       safeAreaEdges={["top"]}
       contentContainerStyle={themed($container)}
       preset="auto"
-      ScrollViewProps={{ scrollEnabled: true }}
+      ScrollViewProps={{
+        scrollEnabled: true,
+        stickyHeaderIndices: [0],
+        StickyHeaderComponent: () => <ExploreStickyHeader logo={passryLogoText} />,
+        invertStickyHeaders: true,
+      }}
     >
-      <ExploreStickyHeader />
-      <Text text="Home screen" />
+      <View>
+        {Array(25).map((_, index) => (
+          <Text key={index} text="Home screen" />
+        ))}
+      </View>
     </Screen>
   )
 }
 
-const $container: ThemedStyle<ViewStyle> = ({ colors }) => ({
-  flex: 1,
+const $container: ThemedStyle<ViewStyle> = ({ colors, spacing }) => ({
+  // flex: 1,
   backgroundColor: colors.background,
 })
--- apps/mobile/src/components/StickyHeader/Explore.tsx
+++ apps/mobile/src/components/StickyHeader/Explore.tsx
@@ -1,26 +1,75 @@
-import { ImageStyle, View, ViewStyle } from "react-native"
+import { ImageBackground, ImageStyle, ImageURISource, View, ViewStyle } from "react-native"
 import { AutoImage } from "../AutoImage"
 import { ThemedStyle } from "@/theme"
 import { useAppTheme } from "@/utils/useAppTheme"
+import { PressableIcon } from "../Icon"
+import { Text } from "../Text"
+import { TextField } from "../TextField"
 
-const passryLogoText = require("../../../assets/images/passry-logo-text.png")
+const backgroundImage = require("../../../assets/images/home-hero.png")
 
-export default function ExploreStickyHeader() {
-  const { themed } = useAppTheme()
+export default function ExploreStickyHeader({ logo }: { logo: ImageURISource }) {
+  const { themed, theme } = useAppTheme()
 
   return (
     <View style={themed($stickyContainerStyle)}>
-      <AutoImage src={passryLogoText} style={$passryLogoTextStyle} />
+      <ImageBackground source={backgroundImage} style={$backgroundImageStyle}>
+        <View style={themed($stickyBarStyle)}>
+          <AutoImage source={logo} style={$passryLogoTextStyle} resizeMode="cover" />
+          <View style={{ flexDirection: "row", gap: 10 }}>
+            <PressableIcon
+              icon="notification"
+              size={theme.spacing.xl}
+              style={{ tintColor: "white" }}
+            />
+            <PressableIcon
+              icon="theme-toggle"
+              size={theme.spacing.xl}
+              style={{ tintColor: "white" }}
+            />
+          </View>
+        </View>
+        <View style={{ paddingBottom: theme.spacing.lg }}>
+          <Text text="Welcome, Donn!" preset="heading" style={{ color: "white", margin: 0 }} />
+          <TextField
+            placeholder="Enter info"
+            inputMode="text"
+            secureTextEntry
+            containerStyle={[themed($textInputStyle)]}
+          />
+        </View>
+      </ImageBackground>
     </View>
   )
 }
 
 const $passryLogoTextStyle: ImageStyle = {
-  position: "absolute",
-  top: 2,
-  left: 10,
+  // position: "absolute",
+  // top: 2,
+  // left: 10,
+  width: 140,
+  height: 42,
 }
 
+const $backgroundImageStyle: ViewStyle = {
+  height: "100%",
+  paddingHorizontal: 16,
+  flexDirection: "column",
+  justifyContent: "space-between",
+}
+
+const $stickyBarStyle: ThemedStyle<ViewStyle> = () => ({
+  width: "100%",
+  flexDirection: "row",
+  alignItems: "center",
+  justifyContent: "space-between",
+  paddingVertical: 10,
+})
 const $stickyContainerStyle: ThemedStyle<ViewStyle> = ({ colors }) => ({
   backgroundColor: colors.background,
+  minHeight: "33%",
+})
+const $textInputStyle: ThemedStyle<ViewStyle> = ({ spacing }) => ({
+  width: "100%",
+  marginTop: spacing.xs,
 })
