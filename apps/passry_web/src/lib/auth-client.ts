85b532456249ca77a46917bd9a9c39d5e866384b
Miracle Friday <miraclef60@gmail.com>
2025-07-28 01:52:00 +0100

    refactor: Add emailOTPClient plugin to auth-client

--- apps/passry_web/src/lib/auth-client.ts
+++ apps/passry_web/src/lib/auth-client.ts
@@ -1,12 +1,13 @@
 import { env } from "@/env";
 import { createAuthClient } from "better-auth/react";
-import {phoneNumberClient} from "better-auth/client/plugins"
+import {phoneNumberClient, emailOTPClient} from "better-auth/client/plugins"
 
 export const authClient = createAuthClient({
   baseURL: env.VITE_API_URL,
   basePath: "/auth",
 
   plugins: [
-    phoneNumberClient()
+    phoneNumberClient(),
+    emailOTPClient()
   ]
 })
