594389cef4aa7f9d4d1f52e7d2b46f7ffb970993
Miracle Friday <miraclef60@gmail.com>
2025-07-29 10:29:02 +0100

    chore: Refactor

--- packages/api/drizzle.config.ts
+++ packages/api/drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "drizzle-kit";
 import env from "@/env";
 
 export default defineConfig({
-  schema: env.NODE_ENV === "development"? "./src/db/schema/*" : "./dist/src/db/schema/*",
+  schema: "./dist/src/db/schema/*",
   out: env.NODE_ENV === "development"? "./src/db/migrations" : "./dist/src/db/migrations",
   dialect: env.NODE_ENV === "development" || env.NODE_ENV === "test"? "sqlite" : "turso",
   // @ts-ignore
