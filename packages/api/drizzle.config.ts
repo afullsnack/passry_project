c9583ddfd496c26c126863d79a263e9e868464cf
Miracle Friday <miraclef60@gmail.com>
2025-07-29 08:06:23 +0100

    refactor: Upadte drizzle.config.ts

--- packages/api/drizzle.config.ts
+++ packages/api/drizzle.config.ts
@@ -5,8 +5,8 @@ import env from "@/env";
 export default defineConfig({
   schema: "./src/db/schema",
   out: "./src/db/migrations",
-  dialect: "sqlite",
-  driver: "turso",
+  dialect: env.NODE_ENV === "development" || env.NODE_ENV === "test"? "sqlite" : "turso",
+  // @ts-ignore
   dbCredentials: {
     url: env.DATABASE_URL,
     authToken: env.DATABASE_AUTH_TOKEN,
