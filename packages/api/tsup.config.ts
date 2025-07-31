759d76f2e70f48f61c58da3d74a707b29ca62a8e
Miracle Friday <miraclef60@gmail.com>
2025-07-31 15:25:52 +0100

    refactor: Configure tsup.config.ts to build api package
    - Add exports to package.json
    - Install workspace packages

--- packages/api/tsup.config.ts
+++ packages/api/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["./src/index.ts"],
+  entry: ["./src/app.ts"],
   format: ["esm", "cjs"],
   dts: true,
   outExtension({ format }) {
