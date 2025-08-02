c4800ba88a2eb9b472176bac25355292357ac91c
Miracle Friday <miraclef60@gmail.com>
2025-08-02 10:36:54 +0100

    refactor: Remove import of package.json for version

--- packages/api/src/lib/configure-open-api.ts
+++ packages/api/src/lib/configure-open-api.ts
@@ -2,13 +2,11 @@ import { Scalar } from "@scalar/hono-api-reference";
 
 import type { AppOpenAPI } from "./types";
 
-import packageJSON from "../../package.json" with { type: "json" };
-
 export default function configureOpenAPI(app: AppOpenAPI) {
   app.doc("/doc", {
     openapi: "3.0.0",
     info: {
-      version: packageJSON.version,
+      version: "0.0.1",
       title: "Passry API",
     },
   });
