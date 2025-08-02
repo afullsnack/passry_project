3e19d4eb73218fbc46fa2e1c513da1cddea6112f
Miracle Friday <miraclef60@gmail.com>
2025-08-02 10:52:53 +0100

    chore: Updated build configuration

--- packages/api/drizzle.config.ts
+++ packages/api/drizzle.config.ts
@@ -4,7 +4,7 @@ import env from "@/env";
 
 export default defineConfig({
   schema: env.NODE_ENV === "development" ? "./src/db/schema" : "./dist/src/db/schema",
-  out: env.NODE_ENV === "development" ? "./src/db/migrations" : "./dist/src/db/migrations",
+  out: env.NODE_ENV === "development" ? "./src/db/migrations" : "./dist/src/db/prod/migrations",
   dialect: env.NODE_ENV === "development" ? "sqlite" : "turso",
   dbCredentials: {
     url: env.DATABASE_URL,
