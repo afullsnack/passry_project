71f7559d3168db0944305e586e845e147a27262c
Miracle Friday <miraclef60@gmail.com>
2025-08-02 12:39:47 +0100

    chore: Update build tsup.config.ts

--- packages/api/tsup.config.ts
+++ packages/api/tsup.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["./src/app.ts", "./src/lib/auth.ts"],
+  entry: [
+    "./src/app.ts",
+    "./src/lib/auth.ts",
+    "./drizzle.config.ts",
+  ],
   format: ["esm", "cjs"],
   dts: true,
   outExtension({ format }) {
-    return format === "esm" ? { js: ".mjs" } : { js: ".cjs" };
+    return format === "esm" ? { js: ".mjs" } : { js: ".js" };
   },
 });
