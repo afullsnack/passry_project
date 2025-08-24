e31ca6e15a2e7a611664df1311dbddcd6bd55fbb
Miracle Friday <miraclef60@gmail.com>
2025-08-24 16:04:49 +0100

    feat: Add event datetime dialog

--- apps/passry_web/src/routes/_authed/-components/events/dialogs/event-datetime.tsx
+++ apps/passry_web/src/routes/_authed/-components/events/dialogs/event-datetime.tsx
@@ -17,29 +17,28 @@ import {
 } from '@/components/ui/shadcn-io/mini-calendar'
 import { useIsMobile } from '@/hooks/use-mobile'
 import { useCallback, useRef } from 'react'
+import { cn } from '@/lib/utils'
 
 interface IProps {
   name: string // form field name
   form: any
+  defaultStateDate?: Date
 }
 
-export default NiceModal.create(({ name, form }: IProps) => {
+export default NiceModal.create(({ name, form, defaultStateDate }: IProps) => {
   const modal = useModal()
   const isMobile = useIsMobile()
 
   let timeout: NodeJS.Timeout | null = null
   const handleLongPress = useCallback(
     (event: React.MouseEvent<HTMLButtonElement, MouseEvent> | TouchEvent) => {
-      console.log('Mouse event', event)
       if (event.type === 'mousedown') {
         timeout = setTimeout(() => {
           alert('Waited 1 second')
         }, 2000)
-        console.log(timeout, 'Timout object')
       }
 
       if (event.type === 'mouseup') {
-        console.log(timeout, 'Timout object')
         if (timeout !== null) {
           console.log('Cleared')
           clearTimeout(timeout)
@@ -68,13 +67,17 @@ export default NiceModal.create(({ name, form }: IProps) => {
         </DialogHeader>
         <div className="space-y-4">
           <form.Field
-            name={name || 'title'}
+            name={name || 'fieldName'}
             children={(field: AnyFieldApi) => (
               <>
                 <Label>Event Date and Time</Label>
                 <MiniCalendar
-                  onValueChange={field.handleChange}
+                  onValueChange={(date) => {
+                    console.log('Date selected:', date)
+                    field.handleChange(date)
+                  }}
                   value={field.state.value}
+                  defaultStartDate={defaultStateDate}
                   days={isMobile ? 3 : 5}
                   className="flex items-center justify-around w-full"
                 >
@@ -94,6 +97,30 @@ export default NiceModal.create(({ name, form }: IProps) => {
                   </MiniCalendarDays>
                   <MiniCalendarNavigation direction="next" />
                 </MiniCalendar>
+                <Input
+                  type="time"
+                  id="time-picker"
+                  step="1"
+                  defaultValue="10:30:00"
+                  onChange={(e) => {
+                    console.log('Time selected', e.target.valueAsDate)
+                    const prevDate = field.state.value as Date
+                    const newDate = new Date(
+                      prevDate.setHours(
+                        e.target.valueAsDate?.getHours() || 0,
+                        e.target.valueAsDate?.getMinutes() || 0,
+                        e.target.valueAsDate?.getSeconds() || 0,
+                        e.target.valueAsDate?.getMilliseconds() || 0,
+                      ),
+                    )
+                    field.handleChange(newDate)
+                    console.log('New date time', newDate)
+                  }}
+                  className={cn(
+                    'bg-background appearance-none [&::-webkit-calendar-picker-indicator]:hidden [&::-webkit-calendar-picker-indicator]:appearance-none',
+                    '!text-3xl font-sans font-semibold size-[35%] mx-auto items-center justify-center',
+                  )}
+                />
               </>
             )}
           />
