ccde083e9d1aa0839e5ffe9654c2b44471fbaa0b
Miracle Friday <miraclef60@gmail.com>
2025-08-26 08:59:51 +0100

    refactor: Fix category, removed dialog and made combobox display in new event page

--- apps/passry_web/src/routes/_authed/-components/events/dialogs/event-category.tsx
+++ apps/passry_web/src/routes/_authed/-components/events/dialogs/event-category.tsx
@@ -1,21 +1,4 @@
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-} from '@/components/ui/dialog'
-import { Button } from '@/components/ui/button'
-import NiceModal, { useModal } from '@ebay/nice-modal-react'
 import type { AnyFieldApi } from '@tanstack/react-form'
-import { Label } from '@/components/ui/label'
-import { Input } from '@/components/ui/input'
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select'
 import {
   Popover,
   PopoverContent,
@@ -32,6 +15,7 @@ import {
 } from '@/components/ui/command'
 import { cn } from '@/lib/utils'
 import { useState } from 'react'
+import { Badge } from '@/components/ui/badge'
 
 interface IProps {
   name: string // form field name
@@ -51,83 +35,87 @@ const defaultCategories = [
   'Other',
 ]
 
-export default NiceModal.create(({ name, form, defaultValue }: IProps) => {
-  const modal = useModal()
+export default function CategorySelect({ name, form, defaultValue }: IProps) {
   const [open, setOpen] = useState(false)
-  const [value, setValue] = useState('')
+  const [value, setValue] = useState(defaultValue)
 
   return (
-    <Dialog
-      open={modal.visible}
-      onOpenChange={(open) => (open ? modal.show() : modal.hide())}
-      modal
-    >
-      <DialogContent>
-        <DialogHeader>
-          <DialogDescription className="text-start">
-            Pick a category for your event or create a new one.
-          </DialogDescription>
-        </DialogHeader>
-        <form.Field
-          name={name || 'title'}
-          children={(field: AnyFieldApi) => (
-            <>
-              <Label>Event category</Label>
-              <Popover open={open} onOpenChange={setOpen}>
-                <PopoverTrigger asChild>
-                  <Button
-                    variant="outline"
-                    role="combobox"
-                    aria-expanded={open}
-                    className="w-[200px] justify-between"
-                  >
-                    {value
-                      ? defaultCategories.find((category) => category === value)
-                      : 'Select category...'}
-                    <ChevronsUpDown className="opacity-50" />
-                  </Button>
-                </PopoverTrigger>
-                <PopoverContent className="w-[200px] p-0">
-                  <Command>
-                    <CommandInput
-                      placeholder="Search framework..."
-                      className="h-9"
-                    />
-                    <CommandList>
-                      <CommandEmpty>No Category found.</CommandEmpty>
-                      <CommandGroup>
-                        {defaultCategories.map((category) => (
-                          <CommandItem
-                            key={category}
-                            value={category}
-                            onSelect={(currentValue) => {
-                              setValue(
-                                currentValue === value ? '' : currentValue,
-                              )
-                              setOpen(false)
-                            }}
-                          >
-                            {category}
-                            <Check
-                              className={cn(
-                                'ml-auto',
-                                value === category
-                                  ? 'opacity-100'
-                                  : 'opacity-0',
-                              )}
-                            />
-                          </CommandItem>
-                        ))}
-                      </CommandGroup>
-                    </CommandList>
-                  </Command>
-                </PopoverContent>
-              </Popover>
-            </>
-          )}
-        />
-        <Button onClick={() => modal.hide()}>Set category</Button>
-      </DialogContent>
-    </Dialog>
+    // <Dialog
+    //   open={modal.visible}
+    //   onOpenChange={(open) => (open ? modal.show() : modal.hide())}
+    //   modal
+    // >
+    //   <DialogContent className='min-h-[]'>
+    //     <DialogHeader>
+    //       <DialogDescription className="text-start">
+    //         Pick a category for your event or create a new one.
+    //       </DialogDescription>
+    //     </DialogHeader>
+    <form.Field
+      name={name || 'category'}
+      children={(field: AnyFieldApi) => (
+        <>
+          <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+              <Badge
+                className="rounded-2xl lg:h-6 flex gap-3 justify-between capitalize"
+                variant="secondary"
+              >
+                {value ? value : 'Select category...'}
+                <ChevronsUpDown className="opacity-50" />
+              </Badge>
+              {/*<Button
+                variant="outline"
+                role="combobox"
+                // aria-expanded={open}
+                className="w-[200px] justify-between capitalize"
+              >
+                {value ? value : 'Select category...'}
+                <ChevronsUpDown className="opacity-50" />
+              </Button>*/}
+            </PopoverTrigger>
+            <PopoverContent className="w-[200px] p-0">
+              <Command>
+                <CommandInput
+                  placeholder="Search category..."
+                  className="h-9"
+                />
+                <CommandList>
+                  <CommandEmpty>No Category found.</CommandEmpty>
+                  <CommandGroup>
+                    {defaultCategories.map((category) => (
+                      <CommandItem
+                        key={category.toLowerCase()}
+                        value={category.toLowerCase()}
+                        onSelect={(currentValue) => {
+                          setValue(
+                            currentValue === value ? '' : currentValue, // deselect if value was previously selected
+                          )
+                          field.handleChange(currentValue)
+                          // setOpen(false)
+                        }}
+                      >
+                        {category}
+                        <Check
+                          className={cn(
+                            'ml-auto',
+                            value === category.toLowerCase()
+                              ? 'opacity-100'
+                              : 'opacity-0',
+                          )}
+                        />
+                      </CommandItem>
+                    ))}
+                  </CommandGroup>
+                </CommandList>
+              </Command>
+            </PopoverContent>
+          </Popover>
+        </>
+      )}
+    />
+    //     <Button onClick={() => modal.hide()}>Set category</Button>
+    //   </DialogContent>
+    // </Dialog>
   )
-})
+}
