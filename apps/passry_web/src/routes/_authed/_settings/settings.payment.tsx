f669e85d89ef80dc15b6a8f3260988d9e101692e
Miracle Friday <miraclef60@gmail.com>
2025-09-03 08:04:16 +0100

    refactor: Add initial layout to payments settings tab

--- apps/passry_web/src/routes/_authed/_settings/settings.payment.tsx
+++ apps/passry_web/src/routes/_authed/_settings/settings.payment.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_authed/_settings/settings/payment')({
@@ -5,5 +6,17 @@ export const Route = createFileRoute('/_authed/_settings/settings/payment')({
 })
 
 function RouteComponent() {
-  return <div>Hello "/_authed/settings/settings/payments"!</div>
+  return (
+    <div>
+      <div className="rounded-lg overflow-hidden mb-12 py-6">
+        <h2 className="text-2xl font-semibold mb-2">Payment Methods</h2>
+        <p className="text-gray-400">
+          Your saved payment methods are encrypted and stored by paystack.
+        </p>
+        <div className="flex my-6">
+          <Button>Add payment method</Button>
+        </div>
+      </div>
+    </div>
+  )
 }
